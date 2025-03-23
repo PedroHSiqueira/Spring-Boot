45c333ef0e8ff0356c6350c4dc382bce1dcc4db8
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2025-03-23 12:18:39 -0300

    Feat: add mais verificações ao AutorDTO

--- Modulo C - JPA & API Restful/src/main/java/io/github/cursospringboot/libraryapi/controller/dto/AutorDTO.java
+++ Modulo C - JPA & API Restful/src/main/java/io/github/cursospringboot/libraryapi/controller/dto/AutorDTO.java
@@ -3,12 +3,24 @@ package io.github.cursospringboot.libraryapi.controller.dto;
 import io.github.cursospringboot.libraryapi.model.Autor;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 //Data-Transfer-Object
-public record AutorDTO(UUID id, @NotBlank(message = "Campo Obrigatório") String nome, @NotNull(message = "Campo Obrigatório") LocalDate dataNascimento, @NotBlank(message = "Campo Obrigatório") String nacionalidade) {
+public record AutorDTO(
+        UUID id,
+        @NotBlank(message = "Campo Obrigatório")
+        @Size(min = 5, max = 100, message = "Fora do tamanho padrão")
+        String nome,
+        @NotNull(message = "Campo Obrigatório")
+        @Past(message = "Não pode ser uma data futura")
+        LocalDate dataNascimento,
+        @NotBlank(message = "Campo Obrigatório")
+        @Size(min = 5, max = 50, message = "Fora do tamanho padrão")
+        String nacionalidade) {
 
     public Autor mapearAutor(){
         Autor autor = new Autor();
