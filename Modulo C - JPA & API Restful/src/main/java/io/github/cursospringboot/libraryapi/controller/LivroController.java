67df659b52de031cc4dd543232203afc4d42637f
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2025-03-25 20:45:53 -0300

    Feat: Criando Método salvar Livro

--- Modulo C - JPA & API Restful/src/main/java/io/github/cursospringboot/libraryapi/controller/LivroController.java
+++ Modulo C - JPA & API Restful/src/main/java/io/github/cursospringboot/libraryapi/controller/LivroController.java
@@ -1,13 +1,30 @@
 package io.github.cursospringboot.libraryapi.controller;
 
+import io.github.cursospringboot.libraryapi.controller.dto.CadastroLivroDTO;
+import io.github.cursospringboot.libraryapi.controller.dto.ErroResposta;
+import io.github.cursospringboot.libraryapi.exceptions.RegistroDuplicadoException;
 import io.github.cursospringboot.libraryapi.service.LivroService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/livro")
+@RequestMapping("/livros")
 @RequiredArgsConstructor
 public class LivroController {
     private final LivroService livroService;
+
+    @PostMapping
+    public ResponseEntity<Object> salvar(@RequestBody @Valid CadastroLivroDTO dto){
+        try {
+            return ResponseEntity.ok(dto);
+        }catch (RegistroDuplicadoException e){
+            ErroResposta erroDTO = ErroResposta.conflito(e.getMessage());
+            return ResponseEntity.status(erroDTO.status()).body(erroDTO);
+        }
+    }
 }
--- Modulo C - JPA & API Restful/src/main/java/io/github/cursospringboot/libraryapi/controller/dto/CadastroLivroDTO.java
+++ Modulo C - JPA & API Restful/src/main/java/io/github/cursospringboot/libraryapi/controller/dto/CadastroLivroDTO.java
@@ -1,17 +1,27 @@
 package io.github.cursospringboot.libraryapi.controller.dto;
 
 import io.github.cursospringboot.libraryapi.model.GeneroLivro;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import org.hibernate.validator.constraints.ISBN;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record CadastroLivroDTO(
+        @ISBN
+        @NotBlank(message = "Campo obrigatório")
         String isbn,
+        @NotBlank(message = "Campo obrigatório")
         String titulo,
+        @NotNull(message = "Campo obrigatório")
+        @PastOrPresent
         LocalDate dataPublicacao,
         GeneroLivro genero,
         BigDecimal preco,
+        @NotNull(message = "Campo obrigatório")
         UUID idAutor
 ) {
 }
